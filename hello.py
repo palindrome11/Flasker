3a3ee9c80d7ae16634487e0a155a0fcfb0f04322
Chris Conlon <cwconlon@gmail.com>
2021-07-12 12:54:01 -0700

    initial commit of Flasker

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Create a route decorator
+@app.route('/')
+def index():
+    return "<h1>Hello World!</h1>"
+
+@app.route('/user/<name>')
+def user(name):
+    return "<h1>Hello {} !!!".format(name)
+
+
+
+app.run()
+
