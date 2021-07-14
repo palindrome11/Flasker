d9a00702dc958ca7e0f326280b44204f55b746ad
Christopher Conlon <cwconlon@gmail.com>
2021-07-13 22:58:39 -0700

    Added template directory

--- hello.py
+++ hello.py
@@ -6,12 +6,31 @@ app = Flask(__name__)
 
 # Create a route decorator
 @app.route('/')
+# def index():
+#     return "<h1>Hello World!</h1>"
+
+###Filters
+# safe
+# capitalize
+# lower
+# upper
+# title
+# trim
+# striptage
+
+
 def index():
-    return "<h1>Hello World!</h1>"
+    first_name="Chris"
+    stuff = "This is <strong>Bold Text</strong>"
+
+    favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
+    return render_template("index.html",first_name=first_name,stuff=stuff,favorite_pizza=favorite_pizza)
 
 @app.route('/user/<name>')
 def user(name):
-    return "<h1>Hello You Rockin' Cool Dude -> {} !!!".format(name)
+    #    return "<h1>Hello You Rockin' Cool Dude -> {} !!!".format(name)
+    return render_template("user.html",name=name)
+
 
 
 
